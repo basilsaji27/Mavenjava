4d26598273c6f549402a0eac9dbb6ccef15347bd
acer <acer@BASIL>
2025-06-26 08:57:43 +0530

    Hello World

---
+++ javamaven/src/main/java/javamavenpackage1/SampleMavenProject1.java
@@ -0,0 +1,10 @@
+package javamavenpackage1;
+
+public class SampleMavenProject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+	}
+
+}
